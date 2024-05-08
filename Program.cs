2ce003b14cb5de608f2c70fe27fa93de480c5b27
LeAnhQuan91 <165478725+LeAnhQuan91@users.noreply.github.com>
2024-05-08 20:32:14 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Giai phuong trinh bac 1");
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("Phuong trinh co dang ax + b = 0");
+
+        Console.WriteLine("Nhap vao so a:");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Nhap vao so b:");
+        double b = Convert.ToDouble(Console.ReadLine());
+if(a != 0)
+{
+    Console.WriteLine($"Phuong trinh co nghiem la: {-b}/{a}");
+}
+else
+{
+    if(b != 0)  
+    {
+        Console.WriteLine("phuong trinh vo nghiem");
+    }
+    else
+    {
+        Console.WriteLine("phuong trinh luon dung");
+    }
+}
+        
+    }
+    
+}
